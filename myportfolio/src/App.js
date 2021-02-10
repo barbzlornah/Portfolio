8915d188c88c17a1ca4aaf994d5094a4e1cc9821
Barbara Masinde <barbzlornah@gmail.com>
2021-02-10 17:24:56 +0300

    new commit

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,11 +1,73 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Header from "./Header";
+import Particles from "react-particles-js";
+import Section from "./components/Section";
+import resume from "./Resume";
+import aboutMe from "./Aboutme";
+import projects from "./Projects";
+import contacts from "./Contacts";
 
 function App() {
   return (
+    <>
+    <Particles
+    className = "particles-canvas"
+     params = {{
+       particles:  {
+         number: {
+           value:30,
+           density: {
+             enable: true,
+             value_area: 900
+           }
+
+         },
+         shape: {
+           type: "circle",
+           stroke: {
+             width: 6,
+             color: "#f9ab00"
+           }
+         }
+       }
+
+     }}
+    />
     <Navbar/>
-    
+    <Header/>
+        <Section
+          title="Home"
+          subtitle={Header}
+          dark={true}
+          id="header"
+        />
+        <Section
+          title="Resume"
+          subtitle={resume}
+          
+          id="resume"
+        />
+        <Section
+          title="About Me"
+          subtitle={aboutMe}
+          
+          id="aboutme"
+        />
+        <Section
+          title="Projects"
+          subtitle={projects}
+          
+          id="projects"
+        />
+        <Section
+          title="Contacts"
+          subtitle={contacts}
+          
+          id="contacts"
+        />
+    </>
   );
 }
 
--- myportfolio/src/components/Navbar.js
+++ myportfolio/src/components/Navbar.js
@@ -1,47 +1,95 @@
-import React from "react";
-import logo from "../logo.jpg";
+import React, {Component} from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 // REACT FONTAWESOME IMPORTS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-const Navbar = () => {
+export default class Navbar extends Component {
+  scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  render(){
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-<div className = "container">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className = "container">
 
 
-  <a class="navbar-brand" href="#"><img className ="logo" src = { logo } alt ="logo..."/></a>
+  <a className="navbar-brand"></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <FontAwesomeIcon icon = {faBars} style = {{color: "#fff"}}/>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">About Me</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Services</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Experience</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Portfolio</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Contacts</a>
-      </li>
-      
-    </ul>
+    <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="Header"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="resume"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Resume
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="aboutme"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                About Me
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="contacts"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Contacts
+              </Link>
+            </li>
+      </ul>
     
   </div>
 </div>
 </nav>
     )
 }
-export default Navbar;
+
+}
